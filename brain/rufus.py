4cc4c88b3425c896680e5c40547d73f3e2010f4e
Fernando Garcia <odnanref@kubrick-5995.local>
2021-10-18 17:33:02 -0700

    this is sloppy and i should clean it up

--- brain/rufus.py
+++ brain/rufus.py
@@ -34,35 +34,45 @@ def recordAudio():
 
 bye_words = ["exit", "quit", "okay bye", "goodbye", "bye", "see you later", "see ya later", "cya", "ttyl", "ttya"]
 hi_words = ["hello", "Hi there", "hi" "hey", "hello", "hiya", "hey there", "heyya", "hey", "hello there", "hey hey"]
-
+questions = ["who", "what", "why", "where", "when", "how", "can", "will"]
 print("\n" + "#"*20 + " Say something " + "#"*20 + "\n")
 while True:
     words = recordAudio()
     words
     if words in hi_words:
-        os.system(f'{random.choice(hi_words)}')
-        break
+        os.system(f'say -v Daniel "{random.choice(hi_words)}"')
     if words in bye_words:
         print("\n" + "#"*20 + " Bye " + "#"*20 + "\n")
         exit(1)
-    if "hey Rufus open a Google search for" in words:
+
+    elif "hey Rufus open a Google search for" in words:
         words = words.replace("hey Rufus open a Google search for", "")
         endwords = words.strip().replace(" ","+")
         os.system(f'say -v Daniel "searching for {words.strip()}"')
         webbrowser.open(f"https://www.google.com/search?q={endwords}")
-    if "hey Rufus" or "a Rufus" or "Rufus" and "who is" or "when is" or "when did" or "why is" or "what is" in words:
+    elif words.startswith("hey Rufus") or words.startswith("a Rufus") or words.startswith("Rufus") and "who is" or "when is" or "when did" or "why is" or "what is" in words:
         words = words.replace("hey Rufus", "")
         words = words + "?"
         print(words)
         os.system(f'say -v Daniel "searching for {words.strip()}"')
         try:
             os.system(f'say -v Daniel "{wikipedia.summary(words, sentences=1)}."')
-            break
         except wikipedia.exceptions.DisambiguationError as e:
             cleanup = re.sub(r'^.*?(is|did)', '', words)
             potentials = potentials = wikipedia.search(cleanup)
             os.system(f'say -v Daniel "I found {potentials}. Which one sounds good to you?"')
-            break
-    
+        except wikipedia.exceptions.PageError as e:
+            os.system(f'say -v Daniel "I couldn\'t find anything for {words.strip()}."')
+
+    elif [i for i in questions in words.startswith(i)]:
+            os.system(f'say -v Daniel "searching for {words.strip()}"')
+            try:
+                os.system(f'say -v Daniel "{wikipedia.summary(words, sentences=1)}."')
+            except wikipedia.exceptions.DisambiguationError as e:
+                cleanup = re.sub(r'^.*?(is|did)', '', words)
+                potentials = potentials = wikipedia.search(cleanup)
+                os.system(f'say -v Daniel "I found {potentials}. Which one sounds good to you?"')
+            except wikipedia.exceptions.PageError as e:
+                os.system(f'say -v Daniel "I couldn\'t find anything for {words.strip()}."')
     else:
         os.system(f'say -v Daniel "I couldn\'t find anything on that."')
